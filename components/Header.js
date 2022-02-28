d57ffc40a7014f25ebb0cb08f1b7a279df877d74
briandev25 <briankips34@gmail.com>
2022-02-28 20:52:49 +0300

    Added DateRangePicker from  react-date-range

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,27 @@
 import Image from 'next/image'
-import React from 'react'
+import React,{ useState } from 'react'
 import { SearchIcon,MenuIcon,UserCircleIcon,GlobeAltIcon} from '@heroicons/react/solid'
 
+import 'react-date-range/dist/styles.css'; 
+import 'react-date-range/dist/theme/default.css'; 
+
+import { DateRangePicker } from 'react-date-range';
+
 function Header() {
+const [searchInput,setSearchInput] = useState('');
+const [startDate,setStartDate] = useState(new Date());
+const [endDate,setEndDate] = useState(new Date());
+
+const selectionRange ={
+  startDate,
+  endDate,
+  key:'selection'
+}
+
+const handleSelect = (ranges) =>{
+      setStartDate(ranges.selection.startDate);
+      setEndDate(ranges.selection.endDate);
+}
   return (
     <header className='sticky grid grid-cols-1 md:grid-cols-3 justify-between  top-0 z-50 bg-white shadow-md p-5 md:px-10'>
      {/* left  */}
@@ -11,7 +30,8 @@ function Header() {
      </div>
      {/* center */}
      <div className='flex flex-1 items-center border-2 md:shadow-sm rounded-full relative px-5 py-3 md:p-0 bg-gray-200 md:bg-transparent '>
-         <input type='text' placeholder='Start your search' className='flex-grow pl-5 rounded-full placeholder:text-black placeholder:font-semibold placeholder:text-center outline-none bg-transparent ' />
+         <input type='text' placeholder='Start your search' value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
+          className='flex-grow pl-5 rounded-full placeholder:text-black placeholder:font-semibold placeholder:text-center outline-none bg-transparent ' />
          <SearchIcon className='hidden md:inline-flex h-8 bg-[#ff385c] text-white rounded-full p-2  cursor-pointer m-2 ' />
      </div>
       {/* right */}
@@ -23,6 +43,13 @@ function Header() {
            <UserCircleIcon className='h-8 text-gray-600 ' />
          </div>
       </div>
+      {searchInput && <div><DateRangePicker
+
+        ranges={[selectionRange]}
+        minDate={new Date()}
+        rangeColors={["#FD5B61"]}
+        onChange={handleSelect}
+         /></div> }
     </header>
   )
 }
