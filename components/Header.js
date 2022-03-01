7be198a5ff0e279b328bc0889b16ebbea8929769
briandev25 <briankips34@gmail.com>
2022-03-01 14:15:17 +0300

    Updated DateRangePicker

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React,{ useState } from 'react'
-import { SearchIcon,MenuIcon,UserCircleIcon,GlobeAltIcon} from '@heroicons/react/solid'
+import { SearchIcon,MenuIcon,UserCircleIcon,GlobeAltIcon,PlusIcon,MinusIcon} from '@heroicons/react/solid'
 
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css'; 
@@ -11,7 +11,8 @@ function Header() {
 const [searchInput,setSearchInput] = useState('');
 const [startDate,setStartDate] = useState(new Date());
 const [endDate,setEndDate] = useState(new Date());
-
+const [noOfAdults,setNoOfAdults] = useState(0);
+const [noOfChildren,setNoOfChildren]=useState(0);
 const selectionRange ={
   startDate,
   endDate,
@@ -43,13 +44,51 @@ const handleSelect = (ranges) =>{
            <UserCircleIcon className='h-8 text-gray-600 ' />
          </div>
       </div>
-      {searchInput && <div><DateRangePicker
-
+      {searchInput && <div className=' flex flex-col col-span-3 mx-auto   mt-2'>
+        <DateRangePicker
+        className='w-full'
         ranges={[selectionRange]}
         minDate={new Date()}
         rangeColors={["#FD5B61"]}
         onChange={handleSelect}
-         /></div> }
+         />
+        <div>
+            <h2 className=' text-lg font-semibold mb-3'>Add Guests</h2>
+            <div className='flex flex-col md:flex-row justify-between px-1 py-6 border-y'>
+                 {/* Adults */}
+                <div className='flex items-center justify-between mb-4 md:mb-0 space-x-7'>
+                  <h3 className=' text-gray-600 text-lg'>Adults</h3>
+                  <div className='flex items-center bg-gray-50 rounded-full p-3 space-x-8'>
+                    <PlusIcon onClick={() =>setNoOfAdults(noOfAdults + 1)} className='h-11 bg-gray-100 rounded-full p-3 text-gray-600 cursor-pointer shadow-md' />
+                     <span>{noOfAdults}</span>
+                    <MinusIcon onClick={() =>{
+                        if(noOfAdults >0){
+                          return setNoOfAdults(noOfAdults-1);
+                        }else{
+                          return setNoOfAdults(0);
+                        }
+                    }}  className='h-11 bg-gray-100 rounded-full p-3 text-gray-600 cursor-pointer shadow-md' />
+                  </div>
+                </div>
+                 {/* children */}
+                <div className='flex items-center justify-between space-x-7'>
+                  <h3 className=' text-gray-600 text-lg'>Children</h3>
+                  <div className='flex items-center bg-gray-50 rounded-full p-3 space-x-8'>
+                    <PlusIcon onClick={() =>setNoOfChildren(noOfChildren + 1)} className='h-11 bg-gray-100 rounded-full p-3 text-gray-600 cursor-pointer shadow-md' />
+                     <span>{noOfChildren}</span>
+                     <MinusIcon onClick={() =>{
+                        if(noOfChildren >0){
+                          return setNoOfChildren(noOfChildren-1);
+                        }else{
+                          return setNoOfChildren(0);
+                        }
+                    }} className='h-11 bg-gray-100 rounded-full p-3 text-gray-600 cursor-pointer shadow-md' />
+                  </div>
+                </div>
+            </div>
+            <div className='flex mt-6'><button onClick={() => setSearchInput('')} className='flex-grow text-gray-500'>Cancel</button><button className='flex-grow text-red-400'>Search</button></div>
+        </div>  
+         </div> }
     </header>
   )
 }
